3299ed8935f45dcb80593d573e7a9f3ba1f64006
Kaspar Schiess <eule@space.ch>
2009-09-06 12:01:20 +0200

    . laying out

--- _helpers.rb
+++ _helpers.rb
@@ -1,9 +1,20 @@
 
 require 'cgi'
+require 'active_support'
 
 module Helpers
   # Links to a tag.
   def tag_link(tag)
     link_to(h(tag), '/tag#' + CGI.escape(tag))
+  end
+  
+  def countify(number)
+    case number
+      when 1: '1st'
+      when 2: '2nd'
+      when 3: '3rd'
+    else
+      "#{number}th"
+    end
   end
 end
